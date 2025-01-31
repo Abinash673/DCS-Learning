508d8b16551c97b1823dd8a7c2a1338788110654
Abinash673 <kumarabinash673@gmail.com>
2025-02-01 03:20:56 +0530

    Updated package.json and Home.jsx

--- src/Pages/Components/Home.jsx
+++ src/Pages/Components/Home.jsx
@@ -11,7 +11,7 @@ import {
   useMediaQuery,
   useTheme,
 } from "@mui/material";
-import hero from "../../assets/hero.avif";
+import hero from "../../assets/hero.jpg";
 import { products, testimonials, socialLinks, images } from "../../Data/Data";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
